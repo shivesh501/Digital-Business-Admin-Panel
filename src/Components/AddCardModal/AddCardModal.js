ad81b6c8e938ac59d738d4318cf2d855d34d81cf
Shivesh K <shivkrish501@gmail.com>
2025-08-11 18:49:02 +0530

    updated the rodal library to react-modal to support react19

--- src/Components/AddCardModal/AddCardModal.js
+++ src/Components/AddCardModal/AddCardModal.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+/*import React, { useState } from "react";
 import css from "./AddCardModal.module.css";
 import Rodal from "rodal";
 
@@ -52,6 +52,86 @@ const AddCardModal = ({ visible, onClose, handleCardAdd }) => {
         </button>
       </div>
     </Rodal>
+  );
+};
+
+export default AddCardModal;
+
+*/
+import React, { useState } from "react";
+import css from "./AddCardModal.module.css";
+import Modal from "react-modal";
+
+// This is required for accessibility reasons
+Modal.setAppElement("#root");
+
+const AddCardModal = ({ visible, onClose, handleCardAdd }) => {
+  const customStyle = {
+    content: {
+      background: "rgb(58 58 58)",
+      padding: "20px",
+      width: "50%",
+      height: "fit-content",
+      maxWidth: "40rem",
+      margin: "auto",
+      borderRadius: "8px",
+      border: "none",
+    },
+    overlay: {
+      backgroundColor: "rgba(0,0,0,0.5)",
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
+    },
+  };
+
+  const [title, setTitle] = useState("");
+  const [detail, setDetail] = useState("");
+
+  const handleAdd = () => {
+    handleCardAdd(title.trim(), detail.trim());
+    setTitle("");
+    setDetail("");
+    onClose();
+  };
+
+  return (
+    <Modal
+      isOpen={visible}
+      onRequestClose={onClose}
+      style={customStyle}
+      shouldCloseOnOverlayClick={true}
+    >
+      <div className={css.container}>
+        <div>
+          <span className={css.label}>Card Title</span>
+          <input
+            type="text"
+            className={css.input}
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+        </div>
+
+        <div>
+          <span className={css.label}>Detail</span>
+          <textarea
+            rows={10}
+            className={css.input}
+            value={detail}
+            onChange={(e) => setDetail(e.target.value)}
+          />
+        </div>
+
+        <button
+          className={css.saveButton}
+          disabled={!title.trim() || !detail.trim()}
+          onClick={handleAdd}
+        >
+          Add
+        </button>
+      </div>
+    </Modal>
   );
 };
 
